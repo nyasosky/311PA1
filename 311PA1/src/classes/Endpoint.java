4991ebce62f179b9c752594f59eadfa6d86ba944
Nathan Yasosky <nyasosky@gmail.com>
2019-03-29 11:42:23 -0500

    JavaDoc and updateNodeValue placement changes

--- 311PA1/src/classes/Endpoint.java
+++ 311PA1/src/classes/Endpoint.java
@@ -1,19 +1,45 @@
 package classes;
 
+/**
+ * Class for an endpoint to be associated with a node
+ * @author Ryan Connolly and Nathan Yasosky
+ *
+ */
 public class Endpoint {
 
+	/**
+	 * Value of the endpoint
+	 */
 	private int value;
+	
+	/**
+	 * Value to determine if the endpoint is a left or right endpoint, 1 for left, -1 for right
+	 */
 	private int p;
 
+	/**
+	 * Constructor for an endpoint object
+	 * Each endpoint will have a value and a p value
+	 * @param value This is the true value that an endpoint will hold
+	 * @param p This value is used to determine if the endpoint is a left or right endpoint, it will be either -1 or 1
+	 */
 	public Endpoint(int value, int p) {
 		this.value = value;
 		this.p = p;
 	}
 
+	/**
+	 * Method for getting the value of an endpoint
+	 * @return The value that the endpoint holds
+	 */
 	public int getValue() {
 		return this.value;
 	}
 
+	/**
+	 * Method for knowing if an endpoint is a left or right endpoint
+	 * @return The p value, 1 for left endpoint, and -1 for right endpoint
+	 */
 	public int getP() {
 		return this.p;
 	}
